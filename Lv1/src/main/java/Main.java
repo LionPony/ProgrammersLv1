67fffd0b4b8bc2044fd8c6ffae30ee40cca48d2b
LionPony <146934556+LionPony@users.noreply.github.com>
2023-10-25 07:24:25 +0900

    add problems(AddDigits, EvenOrOdd, Factorizaion, FindRemainOne)

---
+++ Lv1/src/main/java/Main.java
@@ -0,0 +1,19 @@
+//https://school.programmers.co.kr/learn/challenges?order=acceptance_desc&levels=1&languages=java
+
+import programmers.lv1.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 자릿수 더하기
+        // https://school.programmers.co.kr/learn/courses/30/lessons/12931
+        AddDigits addDigits = new AddDigits();
+
+        // 짝수와 홀수
+        // https://school.programmers.co.kr/learn/courses/30/lessons/12937
+        EvenOrOdd evenOrNum = new EvenOrOdd();
+
+        // 나머지가 1이 되는 수 찾기
+        // https://school.programmers.co.kr/learn/courses/30/lessons/87389
+        FindRemainOne findRemainOne = new FindRemainOne();
+    }
+}
---
+++ Lv1/src/main/java/programmers/lv1/operation/Factorization.java
@@ -0,0 +1,39 @@
+package programmers.lv1.operation;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Factorization {
+    public ArrayList<Integer> factorization(int n){
+        // Factorization
+
+        // Make half factors first.
+        // It can reduce time complexity. O(N) -> O(root N)
+        ArrayList<Integer> factorsHalf = new ArrayList<>();
+        int half = (int) Math.sqrt(n);
+        for(int i = 1; i <= half; i++){
+            if(n%i == 0){
+                factorsHalf.add(i);
+            }
+        }
+
+        // Make other half base on half.
+        ArrayList<Integer> factorsOtherHalf = new ArrayList<>();
+        for (Integer integer : factorsHalf) {
+            factorsOtherHalf.add(n / integer);
+        }
+
+        ArrayList<Integer> allFactors = new ArrayList<>();
+        allFactors.addAll(factorsHalf);
+        allFactors.addAll(factorsOtherHalf);
+
+        allFactors.sort(Comparator.naturalOrder());
+
+        // If square root of n is integer. It duplicates.
+        if(Math.pow(Math.sqrt(n), 2) == n){
+            allFactors.remove(allFactors.size()/2);
+        }
+
+        return allFactors;
+    }
+}
