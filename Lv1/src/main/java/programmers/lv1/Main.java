7f4195d97ee9f5808660cf81ab3cb3c6572602c1
LionPony <146934556+LionPony@users.noreply.github.com>
2023-10-25 06:00:54 +0900

    clear all

--- Lv1/src/main/java/programmers/lv1/Main.java
+++ Lv1/src/main/java/programmers/lv1/Main.java
@@ -1,11 +1,7 @@
 package programmers.lv1;
 
-import Operator.Multiplier;
-
 public class Main {
     public static void main(String[] args) {
-        Multiplier multiplier = new Multiplier();
-        System.out.println(multiplier.Multiply(2,6));
-        System.out.println(multiplier.Multiply(2,3,4));
+
     }
 }
